ed86aba0268cec98dd9699b14c95c130cf2ca5ae
Sranphat8 <654259001@webmail.npru.ac.th>
2025-10-10 20:46:16 +0700

    config image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -106,28 +106,28 @@ export default function Home() {
       {/* hero / carousel */}
       <div className="carousel w-full rounded-2xl overflow-hidden shadow">
         <div id="slide1" className="carousel-item relative w-full h-[220px] sm:h-[280px] md:h-[340px] lg:h-[380px]">
-          <img src="../../public/images/slide1.jpg" alt="slide 1" className="w-full h-full object-cover object-center" />
+          <img src="/images/slide1.jpg" alt="slide 1" className="w-full h-full object-cover object-center" />
           <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
             <a href="#slide4" className="btn btn-circle">❮</a>
             <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full h-[220px] sm:h-[280px] md:h-[340px] lg:h-[380px]">
-          <img src="../../public/images/slide2.jpg" alt="slide 2" className="w-full h-full object-cover object-center" />
+          <img src="/images/slide2.jpg" alt="slide 2" className="w-full h-full object-cover object-center" />
           <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
             <a href="#slide1" className="btn btn-circle">❮</a>
             <a href="#slide3" className="btn btn-circle">❯</a>
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full h-[220px] sm:h-[280px] md:h-[340px] lg:h-[380px]">
-          <img src="../../public/images/slide3.jpg" alt="slide 3" className="w-full h-full object-cover object-center" />
+          <img src="/images/slide3.jpg" alt="slide 3" className="w-full h-full object-cover object-center" />
           <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
             <a href="#slide2" className="btn btn-circle">❮</a>
             <a href="#slide4" className="btn btn-circle">❯</a>
           </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full h-[220px] sm:h-[280px] md:h-[340px] lg:h-[380px]">
-          <img src="../../public/images/slide4.jpg" alt="slide 4" className="w-full h-full object-cover object-center" />
+          <img src="/images/slide4.jpg" alt="slide 4" className="w-full h-full object-cover object-center" />
           <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
             <a href="#slide3" className="btn btn-circle">❮</a>
             <a href="#slide1" className="btn btn-circle">❯</a>
